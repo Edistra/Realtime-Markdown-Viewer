ccc124d7bd0d4f497dc7896a07e875d7aa95342e
Edistra <matthieu.desprez@gmail.com>
2016-03-17 15:30:08 +0100

    add strong test

--- converter/markdown.js
+++ converter/markdown.js
@@ -26,11 +26,21 @@
      return str;
 };
 
+ var parseStrong = function(str) {
+     var strongRegExp = /(~~)(.*?)\1/;
+     var stra = [];
+     while ((stra = strongRegExp.exec(str)) !== null) {
+         str = str.replace(stra[0], '<b>' + stra[2] + '</b>')
+     }
+     return str;
+ };
+
 var markdown = {
   parse: function (str, strict) {
     'use strict';
     str = parseHeadline(str);
     str = parseBold(str);
+    str = parseStrong(str);
     str = parseHorizontaleLine(str);
     return str;
   }

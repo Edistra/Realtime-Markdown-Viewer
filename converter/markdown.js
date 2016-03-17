8220ad48f59bdfbd9cb1ee9f74eac3ac9f687dc9
Edistra <matthieu.desprez@gmail.com>
2016-03-17 16:54:12 +0100

    add code test

--- converter/markdown.js
+++ converter/markdown.js
@@ -8,51 +8,6 @@
    return str;
  };
 
- var parseLink = function(str) {
-     var linkRegExp = /\[([^\[]+)\]\(([^\)]+)\)/;
-     var stra = [];
-     while ((stra = linkRegExp.exec(str)) !== null) {
-         str = str.replace(stra[0], '<a ' + 'href="' + stra[2] + '">' + stra[1] + '</a>');
-     }
-     return str;
- };
-
- var parseHorizontaleLine = function(str) {
-  var horizontalRegExp = /^(?:([\*\-_] ?)+)\1\1$/gm;
-  var stra = [];
-  while ((stra = horizontalRegExp.exec(str)) !== null) {
-    str = str.replace(stra[0], '\n<hr/>\n');
-  }
-  return str;
- };
-
- var parseBold = function(str) {
-    var boldRegExp = /(\*\*)(.*?)\1/;
-     var stra = [];
-     while ((stra = boldRegExp.exec(str)) !== null) {
-        str = str.replace(stra[0], '<strong>' + stra[2] + '</strong>')
-     }
-     return str;
-};
-
- var parseItalic = function(str) {
-     var italicRegExp = /(\*|_)(.*?)\1/;
-     var stra = [];
-     while ((stra = italicRegExp.exec(str)) !== null) {
-         str = str.replace(stra[0], '<i>' + stra[2] + '</i>');
-     }
-     return str;
- };
-
- var parseStrong = function(str) {
-     var strongRegExp = /(~~)(.*?)\1/;
-     var stra = [];
-     while ((stra = strongRegExp.exec(str)) !== null) {
-         str = str.replace(stra[0], '<b>' + stra[2] + '</b>')
-     }
-     return str;
- };
-
  var parseItalic = function(str) {
   var italicRegExp = /(\*|_)(.*?)\1/;
   var stra = [];
@@ -60,7 +15,7 @@
     str = str.replace(stra[0], '<i>' + stra[2] + '</i>')
   }
   return str;
-}
+};
 
 var parseBold = function(str) {
   var boldRegExp = /(\*\*)(.*?)\1/;
@@ -69,7 +24,7 @@ var parseBold = function(str) {
     str = str.replace(stra[0], '<b>' + stra[2] + '</b>')
   }
   return str;
- }
+ };
 
 var parseStrong = function(str) {
   var strongRegExp = /(~~)(.*?)\1/;
@@ -78,7 +33,7 @@ var parseStrong = function(str) {
     str = str.replace(stra[0], '<strong>' + stra[2] + '</strong>')
   }
   return str;
-}
+};
 
 
  var parseHorizontaleLine = function(str) {
@@ -88,7 +43,7 @@ var parseStrong = function(str) {
     str = str.replace(stra[0], '\n<hr/>\n');
   }
   return str;
- }
+ };
 
  var parseLink = function(str) {
   var linkRegExp = /\[([^\[]+)\]\(([^\)]+)\)/;
@@ -97,7 +52,7 @@ var parseStrong = function(str) {
     str = str.replace(stra[0], '<a ' + 'href="' + stra[2] + '">' + stra[1] + '</a>');
   }
   return str;
- }
+ };
 
  var parseNewLine = function(str) {
   var newLineRegExp = /(\n)/;
@@ -106,7 +61,16 @@ var parseStrong = function(str) {
     str = str.replace(stra[0], '<br/>');
   }
   return str;
- }
+ };
+
+ var parseCode = function(str) {
+     var codeRegExp = /`{1}(\w+)`{1}/;
+     var stra = [];
+     while ((stra = codeRegExp.exec(str)) !== null) {
+         str = str.replace(stra[0], '<pre>' + stra[1] + '</pre>');
+     }
+     return str;
+ };
 
 
 var markdown = {
@@ -119,6 +83,7 @@ var markdown = {
     str = parseStrong(str);
     str = parseHorizontaleLine(str);
     str = parseLink(str);
+    str = parseCode(str);
     return str;
   }
 };
